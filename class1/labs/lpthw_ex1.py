b2902a73aa43f0a79f27dfdd0ad83275d9d2be41
ibn-s12mtec2002 <ibn.frazier@mail.citytech.cuny.edu>
2012-02-01 19:09:54 -0500

    my first assignment/020112

---
+++ class1/labs/lpthw_ex1.py
@@ -0,0 +1,7 @@
+print "hello World"
+print "Hello Again"
+print " I like typing this."
+print "This is fun."
+print "Yay, Printing!"
+print "I'd much rasther you 'not'."
+print 'I "said" do not touch this.'
