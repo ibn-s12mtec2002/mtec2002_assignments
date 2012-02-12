80c259c882d068e48ee207ed91a539a7d2d13956
ibn-s12mtec2002 <ibn.frazier@mail.citytech.cuny.edu>
2012-02-12 16:40:17 -0500

    variables are assigned with function:raw_input in it is the question

--- class1/labs/lpthw_ex27.py
+++ class1/labs/lpthw_ex27.py
@@ -1,7 +1,7 @@
-y = raw_input("Namwe? ")
+y = raw_input("Name? ")
 
 age = raw_input("How old are you? ")
 height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
 
-print "So, you're %r old, 5r tall and %r heavy." %(age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
